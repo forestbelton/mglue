cda6b12a7b0c02f77101047dfb8f75255a3a560c
Jahan Addison <jahan.addison@jacata.me>
2013-10-17 10:15:24 -0400

    Issue 3 and 4

--- app.js
+++ app.js
@@ -1,56 +1,62 @@
-var express = require('express');
-var sqlite3 = require('sqlite3');
-var crypto  = require('crypto');
+var express = require('express'),
+    os = require('os'),
+    crypto = require('crypto'),
+    mongo = require('mongodb');
 
-var app = express.createServer();
-var db  = new sqlite3.Database('posts.db');
+mongo.MongoClient.connect(process.env.MONGOHQ_URL, function(err, db) {
+    if(err)
+        throw err;
 
-app.use(express.bodyParser());
-app.use(express.static(__dirname + '/public'));
+    db.createCollection('posts', function(err, coll) {
+        if(err)
+            throw err;
 
-app.use(function(req, res, next) {
-    console.log('[' + new Date() + '] ' + req.socket.remoteAddress + ' - ' + req.originalUrl);
-    next();
-});
-app.set('view engine', 'ejs');
+        var app = express.createServer();
 
-app.post('/new', function(req, res) {
-    if(!req.param('contents', false)) {
-        res.send('Fuck off', 500);
-        return;
-    }
+        app.use(express.bodyParser());
+        app.use(express.static(__dirname + '/public'));
 
-    var contents = req.param('contents'),
-        hash = crypto.createHash('sha1').update(contents + new Date()).digest('hex');
-    db.serialize(function() {
-        db.run('CREATE TABLE IF NOT EXISTS pastes (hash TEXT, contents TEXT)');
-        db.run('INSERT INTO pastes VALUES (?, ?)', hash, contents);
-    });
-
-    res.send(JSON.stringify({ hash: hash }));
-});
-
-app.get('/:hash', function(req, res) {
-    if(!req.params.hash) {
-        res.send('Fuck off', 500);
-        return;
-    }
-
-    var hash = req.params.hash;
-    db.serialize(function() {
-        db.run('CREATE TABLE IF NOT EXISTS pastes (hash TEXT, contents TEXT)');
-        db.get('SELECT contents FROM pastes WHERE hash = ?', hash, function(err, row) {
-            if(typeof row !== 'undefined') {
-                res.render('layout', {contents: row.contents});
-            } else {
-                res.send('Paste not found', 404);
-            }
+        app.use(function(req, res, next) {
+            console.log('[' + new Date() + '] ' + req.socket.remoteAddress + ' - ' + req.originalUrl);
+            next();
         });
+
+        app.set('view engine', 'ejs');
+
+        app.post('/new', function(req, res) {
+            if(req.param('contents', false) === false) {
+                res.send('Fuck off', 500);
+                return;
+            }
+            /* TODO: Handle error on insert here */
+            var contents = req.param('contents'),
+                hash = crypto.createHash('sha1').update(contents + new Date()).digest('hex');
+            coll.insert({hash: hash, contents: contents}, function(err) {
+                res.send(JSON.stringify({ hash: hash }));
+            });
+        });
+
+        app.get('/:hash', function(req, res) {
+            if(!req.params.hash) {
+                res.send('Fuck off', 500);
+                return;
+            }
+            coll.findOne({hash: req.params.hash}, function(err, doc) {
+                if(err) {
+                    res.send('Paste not found', 404);
+                } else {
+                    res.render('layout', {
+                        contents:  doc.contents,
+                        separator: os.EOL
+                    });
+                }
+            });
+        });
+
+        app.get('*', function(req, res) {
+            res.render('layout', {contents: '', separator: os.EOL});
+        });
+
+        app.listen(process.env.PORT);
     });
 });
-
-app.get('*', function(req, res) {
-    res.render('layout', {contents: ''});
-});
-
-app.listen(3000);
--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,35 @@
 ;(function() {
-    var $form  = $('#form-new'),
-        $paste = $('#no-form'),
-        $res   = $('#result');
+    var $form     = $('#form-new'),
+        $paste    = $('#no-form'),
+        $res      = $('#result');
+
+    function send_nl2br(contents, to) {
+        contents.split(/[\r\n]+/).map(function(l) { to.append($('<p>').text(l)); });
+    }
+
+    var $lightbox = function(contents, width, height) {       
+        width     = width  || 600;
+        height    = height || 500;
+
+        var $html = $('<div id="lightbox"><span class="close">x</span></div>');
+        
+        send_nl2br(contents, $html);
+
+        $html.css({
+                left:  ($(window).width() - width)  / 2,
+                top:   (($(window).scrollTop() + ($(window).height)) - height) /2,
+                width:  width,
+                height: height
+            })
+            .insertBefore($form)
+            .fadeIn('fast');
+
+        $('.close').on('click', function() {
+                $html.fadeOut('fast', function(){
+                    $html.remove();
+                });
+            });
+    };
 
     $form.on('submit', function(e) {
         e.preventDefault();
@@ -26,7 +54,8 @@
         .then(function(data) {
             history.pushState(null, 'MGlue', '/' + data.hash);
             $form.hide();
-            $res.text(contents);
+            $res.html('');
+            send_nl2br(contents, $res);
             $paste.show();
             MathJax.Hub.Queue(["Typeset", MathJax.Hub, 'result']);
         });
@@ -41,4 +70,18 @@
         }
         return false;
     });
+
+    $('.preview').on('click', function() {
+        if ($('#lightbox').length) {
+            return false;
+        }
+        var contents = $form.find('[name="contents"]').val();
+        if (contents != '') {
+            $lightbox(contents);
+
+            MathJax.Hub.Queue(["Typeset", MathJax.Hub, 'lightbox']);
+        }
+        return false;
+    });
+
 })();
